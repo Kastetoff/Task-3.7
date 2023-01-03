fa6d0ccb06e5cdf3f6bf741d8e2a6d3ca1fc3c96
Konstantin Sosnovskiy <coolhit74@mail.ru>
2023-01-03 16:43:02 +0300

    Initial commit. 1st task

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        String host = "127.0.0.1";
+        int port = 8080;
+        String clientName = "Andrey Vasiliev";
+
+        try (Socket clientSocket = new Socket(host, port)) {
+            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                out.println(clientName);
+                final String msgFromSrv = in.readLine();
+
+                System.out.println("Connected successful!\n" +
+                        "Message from Server: " + msgFromSrv);
+            }
+        }
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        int port = 8080;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            try (Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                System.out.println("New connection accepted");
+
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+                System.out.println("Client information:\n" +
+                        "\s IP address : " + clientSocket.getInetAddress() + "\n" +
+                        "\s port: " + clientSocket.getPort() + "\n" +
+                        "\s name: " + name);
+            }
+        }
+    }
+}
